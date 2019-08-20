2cd6d2258f7436f2913a79714f0a6b060bce1324
Dmitri Grin <dmtgrin@gmail.com>
2019-08-20 21:35:24 +0300

    Some UI improvements

--- FortuneTeller/Controllers/MainViewController.swift
+++ FortuneTeller/Controllers/MainViewController.swift
@@ -11,17 +11,49 @@ import UIKit
 class MainViewController: UIViewController {
 
     var activityModel: ActivityModel!
+    
+    @IBOutlet weak var questionView: UIView!
+    @IBOutlet weak var answerView: UIView!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
+    var isFlipped = false
+    var spectator = "" {
+        didSet {
+            flip()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         activityModel.delegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        configureViews()
+    }
+    
     @IBAction func getAnswerTapped(_ sender: Any) {
-       activityModel.giveAnswer()
+        activityModel.giveAnswer()
+    }
+    
+    func flip() {
+        isFlipped = !isFlipped
+        let fromView = isFlipped ? questionView : answerView
+        let toView = isFlipped ? answerView : questionView
+        UIView.transition(from: fromView!, to: toView!, duration: 1, options: [.curveEaseOut, .transitionFlipFromLeft, .showHideTransitionViews])
+    }
+    
+    func configureViews() {
+        questionView.layer.cornerRadius = 20
+        questionView.clipsToBounds = true
+        answerView.layer.cornerRadius = 20
+        answerView.clipsToBounds = true
+        containerView.layer.cornerRadius = 20
+        containerView.clipsToBounds = true
     }
 }
+
 
 
 extension MainViewController: ActivityModelProtocol {
@@ -29,10 +61,11 @@ extension MainViewController: ActivityModelProtocol {
     func setAnswer(withAnswer answer: String, forType type: Type) {
         DispatchQueue.main.async {
             self.answerLabel.text = answer
+            self.spectator = answer
             switch type {
-            case .Affirmative: self.answerLabel.backgroundColor = UIColor.green
-            case .Neutral: self.answerLabel.backgroundColor = UIColor.cyan
-            case .Contrary: self.answerLabel.backgroundColor = UIColor.red
+            case .Affirmative: self.answerView.backgroundColor = UIColor.green
+            case .Neutral: self.answerView.backgroundColor = UIColor.cyan
+            case .Contrary: self.answerView.backgroundColor = UIColor.red
             }
         }
     }
